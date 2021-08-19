c66b891ca5ead105d76f5ebb79085ce938c6e73c
Shivam <bhasinshivam2002@gmail.com>
2021-08-19 19:30:13 +0530

    Created the full application

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const app = express();
+
+const Note = require('./models/notes.js');
+
+mongoose.connect('mongodb://localhost:27017/note', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.redirect('/notes');
+});
+
+app.get('/notes', async (req, res) => {
+    const notes = await Note.find({});
+    res.render('index', { notes });
+});
+
+app.post('/notes', async (req, res) => {
+    const { message } = req.body;
+    const note = await new Note({ message: message, canceled: false });
+    note.save();
+    res.redirect('/notes');
+});
+
+app.put('/notes/:id', async (req, res) => {
+    const { message } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { message: message, checked: false });
+    res.redirect('/notes');
+});
+
+app.delete('/notes/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect('/notes');
+});
+
+app.listen('3000', () => {
+    console.log('Server Started');
+});
